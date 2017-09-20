b6fa80f2b9c342c6623953e2ef6441dc1818f82c
mathmanual <mathmanual@163.com>
2017-09-20 18:39:47 +0800

    要提交的变更： 	新文件：   crwtest.py

---
+++ crwtest.py
@@ -0,0 +1,17 @@
+import urllib.request as ur
+from bs4 import BeautifulSoup
+import sys
+import html
+
+if __name__=='__main__':
+    url0 = 'http://www.bjjs.gov.cn/eportal/ui?pageId=393547&systemId=2&categoryId=1&salePermitId=5957669&buildingId=477864'
+    try:
+        html0 = ur.urlopen(url0)
+    except ur.URLError as e:
+        print(e)
+        sys.exit(0)
+    except ur.HTTPError:
+        print(ur.HTTPError)
+        sys.exit(0)
+    bsObj0 = BeautifulSoup(html0)
+    print(str(bsObj0))
